45e03f3de13cce97b692ccd6ae3b6fc646974971
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-09-29 11:04:57 +0200

    created files, etc

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+
+console.log(chalk.blue('Arnaud'));
+console.log(chalk.red('Philippe'));
+console.log(chalk.yellow('Yann'));
+console.log(chalk.green('Vincent'));
